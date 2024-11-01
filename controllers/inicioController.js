02b2bc1aa9adb3e1ee276aac46977f35f3ff7222
Tatiana-Quilindo <lissetht.quilindop@konradlorenz.edu.co>
2024-11-01 15:26:06 -0500

    Implementación de CRUD para Inicio de sesión: creación lógica de controlador para Inicio de sesión

---
+++ controllers/inicioController.js
@@ -0,0 +1,28 @@
+const Registro = require('../models/registro');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+// POST: Iniciar sesión
+exports.login = async (req, res) => {
+    const { correo, contrasena } = req.body;
+
+    try {
+        // Buscar el usuario por correo
+        const usuario = await Registro.findOne({ correo });
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        // Verificar la contraseña
+        const isMatch = await bcrypt.compare(contrasena, usuario.contrasena);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Contraseña incorrecta' });
+        }
+
+        // Generar un token de autenticación
+        const token = jwt.sign({ id: usuario._id }, 'tu_secreto', { expiresIn: '1h' });
+        res.status(200).json({ message: 'Inicio de sesión exitoso', token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
