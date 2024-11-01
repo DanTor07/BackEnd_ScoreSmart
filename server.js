74709787fca598a05bea9a12cbfe88e6b931bccb
Tatiana-Quilindo <lissetht.quilindop@konradlorenz.edu.co>
2024-11-01 14:01:19 -0500

    Se importa la nueva ruta de registroRoutes.js en el archivo server.js para que se registren en el servidor

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const creditoRoutes = require('./routes/creditoRoutes');
+const registroRoutes = require('./routes/registroRoutes');
 
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/creditos', creditoRoutes);
+app.use('/api/registros', registroRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
