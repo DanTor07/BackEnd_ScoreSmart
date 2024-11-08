5dce9ca6ece1bbc914eba045fbfa61a7cf6a1099
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2024-11-08 15:46:51 -0500

    feature: add post method in informacion financiera endpoint

--- controllers/informacionFinancieraController.js
+++ controllers/informacionFinancieraController.js
@@ -10,3 +10,28 @@ exports.getInformacionFinanciera = async (_, res) => {
         res.status(422).json({ error: "Error al obtener la informacion financiera del usuario"})
     }
 };
+
+exports.createInformacionFinanciera = async (req, res) => {
+    const {
+        ingresoNetoMensual,
+        ingresosAdicionales,
+        tipoContratoTrabajo,
+        gastosFijosMensuales,
+        otrosGastos,
+        estadoLaboral,
+        totalActivos,
+        totalPasivos,
+        capacidadAhorro,
+        seguroDeVida
+    } = req.body;
+
+    if ( !ingresoNetoMensual, !ingresosAdicionales, !tipoContratoTrabajo, !gastosFijosMensuales, !otrosGastos, !estadoLaboral, !totalActivos, !totalPasivos, !capacidadAhorro, !seguroDeVida) {
+        return res.status(400).json({ error: "Todos los campos son obligatorios." });
+    }
+
+    const newFinantialInformation = informacionFinancieraSchema(req.body);
+    await newFinantialInformation
+        .save()
+        .then((data) => res.status(201).json(data))
+        .catch((error) => res.status(422).json({ message: "Error en el procesamiento de datos", error: error }));
+};
--- routes/informacionFinancieraRoutes.js
+++ routes/informacionFinancieraRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const informacionFinancieraController = require('../controllers/informacionFinancieraController');
 
-router.get('/', informacionFinancieraController.getInformacionFinanciera)
+router.get('/', informacionFinancieraController.getInformacionFinanciera);
+router.post('/', informacionFinancieraController.createInformacionFinanciera);
+//router.put('/:id', informacionFinancieraController.updateInformacionFinanciera);
+//router.delete('/:id', informacionFinancieraController.deleteInformacionFinanciera)
 
 module.exports = router;
